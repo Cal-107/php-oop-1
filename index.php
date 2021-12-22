8c6d6af685af308a50ffded9e027e8cb027c8fee
“Cal-107” <kaly___7@hotmail.it>
2021-12-22 17:05:27 +0100

    add exercise

--- index.php
+++ index.php
@@ -6,19 +6,45 @@
         public $director;
         public $year;
         public $genre;
+        public $rating;
+        public $isVisible;
 
         // Construct
-        function __contruct($title, $director, $year, $genre) {
+        function __construct($title, $director, $year, $genre, $rating = 18) {
             $this->title = $title;
             $this->director = $director;
             $this->year = $year;
             $this->genre = $genre;
+            $this->rating = $rating;
+            $this->isVisible = $this->ratingLimit($rating);
+        }
+
+        // Methods
+        function ratingLimit($rating) {
+            if ($rating == 18) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        function showMovie() {
+            if ($this->isVisible) {
+                echo 'You can see this movie';
+            } else {
+                echo 'You can\'t see this movie';
+            }
         }
     }
 
     // istances
-    $movie1 = new Movies('Gladiator', 'Ridley Scott', '')
+    $movie1 = new Movies('Gladiator', 'Ridley Scott', '2000', 'epic historical drama', 14);
+    var_dump($movie1);
+    $movie1->showMovie();
 
+    $movie2 = new Movies('The Exorcist', '	William Friedkin', '1973', 'horror',);
+    var_dump($movie2);
+    $movie2->showMovie();
 ?>
 
 
